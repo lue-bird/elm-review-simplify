e419b9f7007dd8714a2a018a7ac64cdb27563565
lue-bird <lue.the.bird@gmail.com>
2025-12-23 09:11:01 +0100

    add List.sort on Set/Dict.toList checks

--- src/Simplify.elm
+++ src/Simplify.elm
@@ -923,6 +923,12 @@ Destructuring using case expressions
     List.sort (List.repeat n a)
     --> List.repeat n a
 
+    List.sort (Set.toList set)
+    --> Set.toList set
+
+    List.sort (Dict.toList dict)
+    --> Dict.toList dict
+
     List.sortBy (always a) list
     --> list
 
@@ -7418,6 +7424,8 @@ listSortChecks =
         , unnecessaryOnWrappedCheck listCollection
         , operationDoesNotChangeResultOfOperationCheck
         , unnecessaryOnSpecificFnCallCheck Fn.List.repeat
+        , unnecessaryOnSpecificFnCallCheck Fn.Set.toList
+        , unnecessaryOnSpecificFnCallCheck Fn.Dict.toList
         ]
 
 
